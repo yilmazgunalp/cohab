2b4d64128d220802ff6f5573c512d5b4bb819d61
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2018-04-16 22:55:43 +1000

    work on authentication

--- helper.js
+++ helper.js
@@ -1,3 +1,7 @@
+
+let {createReadStream,createWriteStream,writeFile,open,write } = require("fs");
+
+
 // retrieves the body of a request. Asynchronous so has to be 'then'ed with a callback
 const getBody = async function(req) {
 	let data = "";
@@ -7,5 +11,43 @@ const getBody = async function(req) {
 	});
 } 
 
+//converts post form data to JSON
+const formToJson = (formdata) => {
+    let result = {}; 
+    formdata.split("&").forEach(e => {
+        let [k,v] = e.split("=");
+        result[k] = v;
+    });
+return result;
+};
 
-module.exports = {getBody};
+const cookiesToJson = (cookies) => {
+    let result = {}; 
+    cookies.split(",").forEach(e => {
+        let [k,v] = e.split("=");
+        result[k] = v;
+    });
+return result;
+}
+
+//reads file and converts to JSON
+const fileToJson = () => {
+	return new Promise(resolve => {  
+	let usersFile = createReadStream("./users.json");
+	let users = "";
+	usersFile.on("data",chunk =>  users += chunk);
+		usersFile.on("end", () => {
+        let result = JSON.parse(("["+ users.replace(/\n/g,",").slice(0,-1) + "]")); 
+	resolve(result);  
+		});
+	});  
+};
+
+
+
+
+
+
+
+module.exports = {getBody,formToJson, fileToJson,cookiesToJson};
+
--- router.js
+++ router.js
@@ -4,7 +4,7 @@ const gethandler = require("./handlers");
 module.exports = function(req) {
  
   let handler = gethandler(parse(req.url).path);
-  console.log(`Received a HTTP request @ ${parse(req.url).path}`);  
+  console.log(`${(new Date(Date.now())).toLocaleTimeString()} Received a HTTP request @ ${parse(req.url).path}`);  
   let qparams = /\?(.*)$/.exec(parse(req.url).path);
   let params;
    if(qparams !== null) {
