b855536355170b74e3637795e38ce1f0d8c3daee
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2018-05-15 00:22:47 +1000

    finish applymiddleware function

--- modules/middlewares.js
+++ modules/middlewares.js
@@ -1,15 +1,31 @@
 const auth = require('./auth')
-const logger = ()=>console.log('I am a middlware');
-const tester = async ({req,resp})=>{
-    console.log('calling tester');
- resp.setHeader('Set-Cookie','ninjas=ruya-baba');
+const logger = async ({req,resp})=> {
+    if(resp.finished) return {req,resp};
+        console.log('LOGGER');
+ resp.setHeader('Tk','T');
     return {req,resp};
+    };
+const tester = async ({req,resp})=>{
+    if(resp.finished) return {req,resp};
+    console.log('TESTER');
+    return new Promise(resolve => {
+        
+setTimeout(()=> {
+    resp.setHeader('Set-Cookie','ninjas=ruya-baba');
+   resolve({req,resp});
+        },5000);
+});
 };
+const drag = async ({req,resp})=> {
+        console.log('DRAG');
+ resp.setHeader('SourceMap','localhost.com');
+    return {req,resp};
+    };
 
 //object that holds list of middlewares for each handler
 const middlewares = {
     login: [auth.loginUser],
-    home: []
+    home: [drag,tester,logger]
     
     };
 //returns the list of middlewares to apply  for given handler
--- modules/mw.js
+++ modules/mw.js
@@ -1,35 +1,17 @@
 const getMw = require('./middlewares');
 
-const runMiddleware = async (mw,result)=> {
-    return await new Promise(resolve => resolve(mw(result)));
-    };
-
-const applyMiddleware = async ({handler,req,resp})=> {
+const applyMiddleware = ({handler,req,resp})=> {
         //get the middlewares for handler
         let middlewares = getMw(handler.name);
         //if there is no mw call handler
-        
-        let promises = [];
         if(!middlewares || middlewares.length == 0 ) {
             handler(req,resp);
-            return;
         }
-        //else iterate over middlewares passing {req,resp} to next one
+        //else reduce over middlewares then call handler with resulting value
         else { 
-
-        let result = {req,resp};
-            middlewares.some((mw)=> {
-                console.log(result);
-                //if resp is already sent break
-                if(resp.finished) return true;
-                //else pass {req,resp} to next middleware
-                result = await runMiddleware(mw,result);
-            });
-        //after applying all middlewares call handler
-            return await handler(req,resp);
-
-    };
-
+        middlewares.reduce((accumulator,current)=>accumulator.then(current),
+        Promise.resolve({req,resp})).then(val => handler(val.req,val.resp));
         };
+    };
 
     module.exports = applyMiddleware;
